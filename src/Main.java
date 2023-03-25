3cd1defcc641ea128d5763df9401e40e45e29c29
Federico Arias <fede.ariass33@gmail.com>
2023-03-25 18:34:29 -0300

    ejercicio 8 / int programacion

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,47 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Persona persona = new Persona();
+
+        persona.setEdad(23);
+        System.out.println(persona.getEdad());
+
+        persona.setNombre("Federico");
+        System.out.println(persona.getNombre());
+
+        persona.setTelefono(22131889);
+        System.out.println(persona.getTelefono());
     }
 }
+
+class Persona{
+    private int edad;
+    private String nombre;
+    private int telefono;
+
+    public void setEdad(int valorEdad){
+        this.edad = valorEdad;
+    }
+    public int getEdad() {
+        return this.edad;
+    }
+
+    public void setNombre(String valorNombre){
+        this.nombre = valorNombre;
+    }
+    public String getNombre(){
+        return this.nombre;
+    }
+
+    public void setTelefono(int valorTelefono){
+        this.telefono = valorTelefono;
+    }
+    public int getTelefono(){
+        return this.telefono;
+    }
+}
+
+// crear clase persona
+// crear variables privadas edad,nombre y telefono de la persona
+// crear gets y sets de cada propiedad
+// crear un objeto persona en el main
+// utilizar gets y sets para darle valores a las propiedades edad, nombre y telefono, por ultimo mostrar en consola
